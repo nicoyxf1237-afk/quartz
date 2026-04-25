8231fdc2400d5df848202354bfe62b32819bc5d4
Nicole <n.i.c.o.yxf1237@gmail.com>
2026-04-25 09:09:32 +0800

    Add title and default state to Explorer component

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,10 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+  title: "目录",
+  folderDefaultState: "open",
+}),
   ],
   right: [
     Component.Graph(),
