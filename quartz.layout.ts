eddb5c3d6160a6947db1971bd0996baeb7c53d90
Nicole <n.i.c.o.yxf1237@gmail.com>
2026-04-25 10:23:08 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,6 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.TagList(),
   ],
   right: [
     Component.Explorer({
